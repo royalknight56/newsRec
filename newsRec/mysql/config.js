068bf689bc333ede3f768d91885e17dc3f9e09ff
royalknight56 <34654733+royalknight56@users.noreply.github.com>
2021-04-11 21:19:08 +0800

    Create config.js

---
+++ newsRec/mysql/config.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: RoyalKnight
+ * @Date: 2021-04-11 21:17:19
+ * @LastEditors: RoyalKnight
+ * @LastEditTime: 2021-04-11 21:18:38
+ */
+module.exports = {
+    appcode:'****************************'//填入阿里云市场中的接口appcode
+}
